86e93fff0c5e300370fc6c221c8c47c5957178b5
bugsounet <github@bugsnana.fr>
2020-04-10 04:37:24 +0200

    installer correct

--- scripts/installer.sh
+++ scripts/installer.sh
@@ -1,5 +1,5 @@
 
-./node_modules/node-pre-gyp/bin/node-pre-gyp clean configure build 2>/dev/null
+./node_modules/node-pre-gyp/bin/node-pre-gyp clean configure 2>/dev/null
 ./node_modules/node-pre-gyp/bin/node-pre-gyp install --build-from-source 2>/dev/null
 ./node_modules/.bin/electron-rebuild
 
